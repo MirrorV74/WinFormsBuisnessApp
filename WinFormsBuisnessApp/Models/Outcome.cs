13097f969117ecc1449e7da2b2cc2ac42e9c7df2
domager73 <112970685+domager73@users.noreply.github.com>
2023-05-12 19:47:36 +0300

    make all folders

---
+++ WinFormsBuisnessApp/Models/Outcome.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsBuisnessApp.Models
+{
+    internal class Outcome
+    {
+        public int Id { get; set; }
+        public DateOnly Date { get; set; }
+        public int CategoryId { get; set; }
+        public int CategoryName { get; set; }
+        public string Description { get; set; }
+        public int Money { get; set; }
+    }
+}
