24ff39446588ea6b07be2595e66833dcd3472929
MirrorV74 <valera_sinitsa@bk.ru>
2023-05-20 17:59:13 +0300

    income and outcome methods created

--- WinFormsBuisnessApp/Controllers/FormMainController.cs
+++ WinFormsBuisnessApp/Controllers/FormMainController.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using WinFormsBuisnessApp.DB;
 using WinFormsBuisnessApp.Models;
+using WinFormsBuisnessApp.Tables;
 
 namespace WinFormsBuisnessApp.Controllers
 {
@@ -21,7 +22,31 @@ namespace WinFormsBuisnessApp.Controllers
 
         public void Test() 
         {
-            List<IncomeCategory> list = _dbManager.TableIncomesCategories.GetAll();
+            //List<IncomeCategory> list = _dbManager.TableIncomesCategories.GetAll();
+
+            //Income income = new Income()
+            //{
+            //    Date = DateOnly.FromDateTime(DateTime.Now),
+            //    CategoryId = 3,
+            //    Description = "aasas",
+            //    Money = 10000
+            //};
+            //_dbManager.TableIncomes.AddNew(income);
+            
+            //Outcome outcome = new Outcome()
+            //{
+            //    Date = DateOnly.FromDateTime(DateTime.Now),
+            //    CategoryId = 3,
+            //    Description = "aasas",
+            //    Money = 10000
+            //};
+            //_dbManager.TableOutcomes.AddNew(outcome);
+            
+            //_dbManager.TableIncomes.DeleteById(1);
+            
+            List<Income> list = _dbManager.TableIncomes.GetByDate(new DateOnly(2023,5,20));
+
+            int i = 5;
         }
     }
 }
--- WinFormsBuisnessApp/Tables/TableOutcomes.cs
+++ WinFormsBuisnessApp/Tables/TableOutcomes.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WinFormsBuisnessApp.Models;
 
 namespace WinFormsBuisnessApp.Tables
 {
@@ -14,6 +15,24 @@ namespace WinFormsBuisnessApp.Tables
         public TableOutcomes(NpgsqlConnection connection)
         {
             _connection = connection;
+        }
+        
+        public void AddNew(Outcome outcome)
+        {
+            string sqlRequest = $"insert into outcomes (outcome_date, outcome_category_id, description, money) values ('{outcome.Date.ToString("yyyy-MM-dd")}',{outcome.CategoryId},'{outcome.Description}',{outcome.Money})";
+
+            NpgsqlCommand command = new NpgsqlCommand(sqlRequest, _connection);
+
+            command.ExecuteNonQuery();
+        }
+        
+        public void DeleteById(int id)
+        {
+            string sqlRequest = $"delete from outcomes where id = {id}";
+
+            NpgsqlCommand command = new NpgsqlCommand(sqlRequest, _connection);
+
+            command.ExecuteNonQuery();
         }
     }
 }
